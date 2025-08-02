73b3be71ef4d75683f246c21ea0d957731afaec2
akash100a <arakash968786@gmail.com>
2025-08-02 14:25:28 +0600

    update

--- app/page.js
+++ app/page.js
@@ -1,20 +1,31 @@
-import Image from "next/image";
-import Container from "./components/Container";
 import Hi from "@/public/hi.png";
 import Newazls from "@/public/newazls.svg";
+import Image from "next/image";
+import Container from "./components/Container";
 
 const page = () => {
-  return <Container className="py-[96px] flex justify-between items-start">
-<div className="mt-[218px]">
-  <h3>
-    <Image src={Hi} alt="Hi" />
-    Hi, I am</h3>
-  <h1 className="hero_heading">Zahid <br /> Newaz 
-  <Image src={Newazls} alt="Newazls" className="inline-block w-[150px] h-[50px]" />
-  </h1>
-</div>
-<div className="">aa</div>
-  </Container>;
+  return (
+    <Container className="py-[96px] flex justify-between items-start">
+      <div className="mt-[218px]">
+        <h3>
+          <Image src={Hi} alt="Hi" />
+          Hi, I am
+        </h3>
+        <div className="hero_heading">
+          Zahid <br />
+          <div className="flex items-end">
+            <h1 className="hero_heading">Newaz</h1>
+            <Image
+              src={Newazls}
+              alt="Newazls"
+              className="inline-block w-[100px] h-[50px]"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="">aa</div>
+    </Container>
+  );
 };
 
 export default page;
