c226a1cba2c0b3c8bfa4a58b6e04417c12c68a82
akash100a <arakash968786@gmail.com>
2025-08-02 13:19:57 +0600

    update

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import Link from "next/link";
+import Container from "./Container";
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Container className="flex justify-between items-center">
+     <div className=""></div>
+      <ul className="flex space-x-4">
+        <li>
+          <Link className="navigation_text" href="/about">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link className="navigation_text" href="/projects">
+            Projects
+          </Link>
+        </li>
+      </ul>
+    </Container>
+  );
+};
 
-export default Header
+export default Header;
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
+        <Header />
         {children}
       </body>
     </html>
