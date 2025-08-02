a80132874ac58787b0d5272812937175470c7c36
akash100a <arakash968786@gmail.com>
2025-08-02 14:08:32 +0600

    update

--- app/page.js
+++ app/page.js
@@ -1,7 +1,17 @@
+import Image from "next/image";
 import Container from "./components/Container";
+import Hi from "@/public/hi.png";
 
 const page = () => {
-  return <Container></Container>;
+  return <Container className="py-[96px] flex justify-between items-start">
+<div className="mt-[218px]">
+  <h3>
+    <Image src={Hi} alt="Hi" />
+    Hi, I am</h3>
+  <h1>Zahid Newaz</h1>
+</div>
+<div className="">aa</div>
+  </Container>;
 };
 
 export default page;
