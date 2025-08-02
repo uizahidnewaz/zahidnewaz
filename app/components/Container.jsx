e7bb27ce232d6eb3851d477c6a006879c87abc72
akash100a <arakash968786@gmail.com>
2025-08-02 22:59:02 +0600

    upp

--- app/components/Container.jsx
+++ app/components/Container.jsx
@@ -1,5 +1,5 @@
 const Container = ({ children, className }) => {
-  return <div className={`max-w-[1226px] px-2 mx-auto ${className}`}>{children}</div>;
+  return <div className={`max-w-[1226px] px-5 md:px-6 mx-auto ${className}`}>{children}</div>;
 };
 
 export default Container;
