f4258e400c4c96ad098605efad3474b38270a217
akash100a <arakash968786@gmail.com>
2025-08-02 14:36:04 +0600

    update

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import Hi from "@/public/hi.png";
+import Hi from "@/public/hi.svg";
 import Newazls from "@/public/newazls.svg";
 import Image from "next/image";
 import Container from "./components/Container";
@@ -7,12 +7,12 @@ const page = () => {
   return (
     <Container className="py-[96px] flex justify-between items-start">
       <div className="mt-[218px]">
-        <h3>
-          <Image src={Hi} alt="Hi" />
-          Hi, I am
-        </h3>
-        <div className="hero_heading">
-          Zahid <br />
+        <div className="flex items-center gap-x-2">
+          <h3 className="hero_title">Hi, I am</h3>
+          <Image className="h-[26px] w-[26px]" src={Hi} alt="Hi" />
+        </div>        <div className="">
+          <h1 className="hero_heading">Zahid</h1>
+          <br />
           <div className="flex items-end">
             <h1 className="hero_heading">Newaz</h1>
             <Image
