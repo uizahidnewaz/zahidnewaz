f701adcfa680442ecded43e9e1b5d79d9d69fe40
akash100a <arakash968786@gmail.com>
2025-08-02 12:09:49 +0600

    update

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
+import * as motion from 'motion/react-client'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
+    <motion.div 
+    initial={{y:20, opacity: 0 }}
+    animate={{y:0, opacity: 1 }}
+    >
       <h1>Hello, Zahid Newaz!</h1>
       <p>Welcome to my portfolio website.</p>
-    </div>
+    </motion.div>
   )
 }
 
