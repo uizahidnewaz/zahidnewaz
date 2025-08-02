a30a119059c22fce608d56b41e6cd5699cb53a3a
akash100a <arakash968786@gmail.com>
2025-08-02 17:53:20 +0600

    update

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
     <motion.div
       initial={{ opacity: 0, y: -100 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.6, ease: [0.25, 0.8, 0.25, 1], delay: 0.2 }}
+      transition={{ duration: 1, ease: [0.25, 0.8, 0.25, 1], delay: 0 }}
     >
       <Container className="flex justify-between items-center py-5 relative">
         {/* Left Side */}
