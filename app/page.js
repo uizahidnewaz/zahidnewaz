7fd6ea8212c94497735a69586a28bf87eeb69a7a
akash100a <arakash968786@gmail.com>
2025-08-02 14:20:47 +0600

    updae

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Container from "./components/Container";
 import Hi from "@/public/hi.png";
+import Newazls from "@/public/newazls.svg";
 
 const page = () => {
   return <Container className="py-[96px] flex justify-between items-start">
@@ -8,7 +9,9 @@ const page = () => {
   <h3>
     <Image src={Hi} alt="Hi" />
     Hi, I am</h3>
-  <h1 className="hero_heading">Zahid <br /> Newaz</h1>
+  <h1 className="hero_heading">Zahid <br /> Newaz 
+  <Image src={Newazls} alt="Newazls" className="inline-block w-[150px] h-[50px]" />
+  </h1>
 </div>
 <div className="">aa</div>
   </Container>;
