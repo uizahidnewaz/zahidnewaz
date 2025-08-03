bc99d1f4d96b36b623763026259c06bf138a1ea7
akash100a <arakash968786@gmail.com>
2025-08-03 18:17:43 +0600

    updatrer

--- app/page.js
+++ app/page.js
@@ -136,21 +136,23 @@ const Page = () => {
               <h3 className="story_title flex-shrink-0">About me</h3>
             </div>
             <div className="">
-
-            <p className="story_pragraph">
-              I'm Shah Md. Zahid Newaz. I studied at Rajuk Uttara Model College,
-              then went on to complete my BSc in Computer Science and
-              Engineering from North South University. For a long time, I
-              thought my path was already set, lines of code, system design,
-              maybe software development.
-            </p>
-            <p className="story_pragraph mt-[30px]"> My journey into design began at the
-              end of 2023, when I discovered a deep interest in creating
-              user-friendly and visually engaging digital experiences. Since
-              then, I’ve been learning and growing every day, exploring UI/UX
-              principles, studying real-world products, and building designs
-              that solve real problems. I believe in continuous learning, and
-              thoughtful design.</p>
+              <p className="story_pragraph">
+                I'm Shah Md. Zahid Newaz. I studied at Rajuk Uttara Model
+                College, then went on to complete my BSc in Computer Science and
+                Engineering from North South University. For a long time, I
+                thought my path was already set, lines of code, system design,
+                maybe software development.
+              </p>
+              <p className="story_pragraph mt-[30px]">
+                {" "}
+                My journey into design began at the end of 2023, when I
+                discovered a deep interest in creating user-friendly and
+                visually engaging digital experiences. Since then, I’ve been
+                learning and growing every day, exploring UI/UX principles,
+                studying real-world products, and building designs that solve
+                real problems. I believe in continuous learning, and thoughtful
+                design.
+              </p>
             </div>
           </div>
           <div className="mt-[84px] flex justify-between items-start gap-x-[80px]">
@@ -159,13 +161,15 @@ const Page = () => {
               <h3 className="story_title flex-shrink-0">My mission</h3>
             </div>
             <p className="story_pragraph">
-             My mission is to make people’s lives easier through thoughtful design and to create experiences that users genuinely enjoy
+              My mission is to make people’s lives easier through thoughtful
+              design and to create experiences that users genuinely enjoy
             </p>
           </div>
-
-
         </div>
       </Container>
+        <div className="makeit py-18 h-[736px]">
+          <Image className="w-full h-full bg-cover" src={Newaz} alt="makeit" />
+        </div>
     </>
   );
 };
