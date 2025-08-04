0a5e5076e9845515a5d6d0dd993380c3dc5b77bd
akash100a <arakash968786@gmail.com>
2025-08-04 11:52:27 +0600

    update

--- app/layers/Experience.jsx
+++ app/layers/Experience.jsx
@@ -5,8 +5,9 @@ const Experience = () => {
   return (
     <Container className="pt-[120px] pb-[96px] flex items-start gap-x-[15px]">
       <h2 className='motivation_heading'>experience</h2>
-      <div className="mt-[202px] flex items-start gap-x-8">
-        <div className="flex-shrink-0 w-[44px] h-[44px] overflow-hidden">
+      <div className="mt-[202px] flex flex-col gap-y-[64px]">
+      <div className="flex items-start gap-x-8">
+          <div className="flex-shrink-0 w-[44px] h-[44px] overflow-hidden">
           <Image className='w-full h-full bg-cover' src={Blinto} alt='blinto' />
         </div>
         <div className="">
@@ -14,6 +15,17 @@ const Experience = () => {
           <h4 className='experience_subheading mt-3 mb-6'>UI/UX Designer</h4>
           <p className='story_pragraph'>I worked closely with developers and directly communicated with clients to design both websites and software. Beyond UI/UX, I also contributed by creating blog graphics that aligned with the company’s content strategy</p>
         </div>
+      </div>
+      <div className="flex items-start gap-x-8">
+          <div className="flex-shrink-0 w-[44px] h-[44px] overflow-hidden">
+          <Image className='w-full h-full bg-cover' src={Blinto} alt='blinto' />
+        </div>
+        <div className="">
+          <h3 className='experience_heading'>Blinto llc<span className='experience_date'> | March 2025 - July 2025</span></h3>
+          <h4 className='experience_subheading mt-3 mb-6'>UI/UX Designer</h4>
+          <p className='story_pragraph'>I worked closely with developers and directly communicated with clients to design both websites and software. Beyond UI/UX, I also contributed by creating blog graphics that aligned with the company’s content strategy</p>
+        </div>
+      </div>
       </div>
     </Container>
   )
