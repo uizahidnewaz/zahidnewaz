64181060f06c04ffb19b7a3e25cdff64870a1196
akash100a <arakash968786@gmail.com>
2025-08-04 18:02:47 +0600

    update

--- app/layers/Stack.jsx
+++ app/layers/Stack.jsx
@@ -1,3 +1,4 @@
+import Canva from "@/public/canva.webp";
 import Figma from "@/public/figma.webp";
 import Image from "next/image";
 import Container from "../components/Container";
@@ -7,9 +8,9 @@ const Stack = () => {
       <h2 className="motivation_heading">Favourite stack</h2>
       <div className="mt-[290px] flex flex-col gap-y-[64px]">
         <div className="flex items-start gap-x-8">
-          <div className="flex-shrink-0 w-[35px] h-[50px] overflow-hidden">
+          <div className="flex-shrink-0 w-[50px] h-[50px] overflow-hidden flex items-center justify-center">
             <Image
-              className="w-full h-full bg-cover"
+              className="h-full w-[70%] bg-cover"
               src={Figma}
               alt="figma"
             />
@@ -20,6 +21,33 @@ const Stack = () => {
             <p className="story_pragraph">
               My go-to tool for UI/UX design, from wireframes to high-fidelity
               prototypes and also for graphic designing
+            </p>
+          </div>
+        </div>
+        <div className="flex items-start gap-x-8">
+          <div className="flex-shrink-0 w-[50px] h-[50px] overflow-hidden">
+            <Image className="w-full h-full bg-cover" src={Canva} alt="figma" />
+          </div>
+          <div className="">
+            <h3 className="stack_heading">Canva</h3>
+            <h4 className="stack_subheading mt-[10px] mb-6">Design Tool</h4>
+            <p className="story_pragraph">
+              I use it for fast, clean, and effective graphic design, especially
+              for social media
+            </p>
+          </div>
+        </div>
+        <div className="flex items-start gap-x-8">
+          <div className="flex-shrink-0 w-[35px] h-[50px] overflow-hidden">
+            <Image className="w-full h-full bg-cover" src={Figma} alt="figma" />
+          </div>
+          <div className="">
+            <h3 className="stack_heading">Notion</h3>
+            <h4 className="stack_subheading mt-[10px] mb-6">
+              For Note-taking and Documentation
+            </h4>
+            <p className="story_pragraph">
+              I rely on it for note-taking, and documenting design systems
             </p>
           </div>
         </div>
