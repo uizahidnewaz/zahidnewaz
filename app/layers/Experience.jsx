c8311070f01ca8c079d5be4c718cf12e9088827d
akash100a <arakash968786@gmail.com>
2025-08-04 11:48:02 +0600

    update

--- app/layers/Experience.jsx
+++ app/layers/Experience.jsx
@@ -10,8 +10,8 @@ const Experience = () => {
           <Image src={Blinto} alt='blinto' />
         </div>
         <div className="">
-          <h3 className='experience_heading'>Blinto llc | March 2025 - July 2025</h3>
-          <h4 className='experience_subheading'>UI/UX Designer</h4>
+          <h3 className='experience_heading'>Blinto llc<span className='experience_date'> | March 2025 - July 2025</span></h3>
+          <h4 className='experience_subheading mt-3 mb-6'>UI/UX Designer</h4>
           <p className='story_pragraph'>I worked closely with developers and directly communicated with clients to design both websites and software. Beyond UI/UX, I also contributed by creating blog graphics that aligned with the company’s content strategy</p>
         </div>
       </div>
