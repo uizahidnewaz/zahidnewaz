df42f2b6b7a97aa7c027dce14754bd4d0c2f63d5
akash100a <arakash968786@gmail.com>
2025-08-04 18:29:14 +0600

    update

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,5 +1,20 @@
 const Footer = () => {
-  return <div>Footerv</div>;
+  return <div className="pt-[180px]">
+    <h2 className="text-center">Let’s connect</h2>
+    <p className="text-center">Thank you for exploring my work and learning a bit about my journey. I’m always open to new opportunities, creative collaborations, or simply chatting about design, tech, or ideas</p>
+    <div className="flex items-center justify-center gap-x-[20px]">
+      <div className="">a</div>
+      <div className="">a</div>
+      <div className="">a</div>
+      <div className="">a</div>
+      <div className="">a</div>
+    </div>
+    <hr />
+    <div className="flex items-center justify-between">
+      <div className="">a</div>
+      <div className="">b</div>
+    </div>
+  </div>;
 };
 
 export default Footer;
