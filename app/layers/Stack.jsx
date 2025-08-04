7daed144b446532a9e3c45d400dc315fe86e5b9f
akash100a <arakash968786@gmail.com>
2025-08-04 12:07:31 +0600

    update

--- app/layers/Stack.jsx
+++ app/layers/Stack.jsx
@@ -16,43 +16,13 @@ const Stack = () => {
             />
           </div>
           <div className="">
-            <h3 className="experience_heading">
-              Blinto llc
-              <span className="experience_date"> | March 2025 - July 2025</span>
-            </h3>
-            <h4 className="experience_subheading mt-3 mb-6">UI/UX Designer</h4>
+            <h3 className="stack_heading">Blinto llc</h3>
+            <h4 className="stack_subheading mt-[10px] mb-6">UI/UX Designer</h4>
             <p className="story_pragraph">
               I worked closely with developers and directly communicated with
               clients to design both websites and software. Beyond UI/UX, I also
               contributed by creating blog graphics that aligned with the
               company’s content strategy
-            </p>
-          </div>
-        </div>
-        <div className="flex items-start gap-x-8">
-          <div className="flex-shrink-0 w-[44px] h-[44px] overflow-hidden">
-            <Image
-              className="w-full h-full bg-cover"
-              src={Btrac}
-              alt="blinto"
-            />
-          </div>
-          <div className="">
-            <h3 className="experience_heading">
-              b-trac solutions ltd
-              <span className="experience_date">
-                {" "}
-                | November 2024 - January 2025
-              </span>
-            </h3>
-            <h4 className="experience_subheading mt-3 mb-6">
-              Graphic & UI/UX Designer Intern
-            </h4>
-            <p className="story_pragraph">
-              I collaborated with the Business Development team to support
-              product promotion through graphic design for social media and
-              marketing materials. I also contributed to the UI/UX team, where I
-              worked on improving digital interfaces
             </p>
           </div>
         </div>
