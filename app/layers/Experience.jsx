aca4ffd9e9459b0d91e392f56f874c7641854dac
akash100a <arakash968786@gmail.com>
2025-08-04 11:31:25 +0600

    update

--- app/layers/Experience.jsx
+++ app/layers/Experience.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-
+import Image from 'next/image'
+import Container from '../components/Container'
+import Blinto from "@/public/blinto.webp"
 const Experience = () => {
   return (
-    <div>
-      Experience
-    </div>
+    <Container className="pt-[120px] pb-[96px] flex items-start gap-x-[15px]">
+      <h2 className='motivation_heading'>experience</h2>
+      <div className="mt-[202px] flex items-start gap-x-8">
+        <div className="w-[44px] h-[44px] overflow-hidden">
+          <Image src={Blinto} alt='blinto' />
+        </div>
+        <div className="">x</div>
+      </div>
+    </Container>
   )
 }
 
