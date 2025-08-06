bfd812284e3ab2ba33386fa05a956391f1efa99e
akash100a <arakash968786@gmail.com>
2025-08-06 19:54:37 +0600

    upp

--- app/layers/AnimatedLink.jsx
+++ app/layers/AnimatedLink.jsx
@@ -12,12 +12,12 @@ const swipeVariants = {
   animate: {
     y: 0,
     opacity: 1,
-    transition: { duration: 0.3, ease: "easeInOut" },
+    transition: { duration: 0.35, ease: [0.25, 0.46, 0.45, 0.94] },
   },
   exit: (direction) => ({
     y: direction > 0 ? -20 : 20,
     opacity: 0,
-    transition: { duration: 0.3, ease: "easeInOut" },
+    transition: { duration: 0.25, ease: [0.55, 0.06, 0.68, 0.19] },
   }),
 };
 
@@ -40,7 +40,7 @@ const AnimatedLink = ({ href, children }) => {
       <a
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
-        className="relative overflow-hidden" // Adjust height based on your font size
+        className="relative overflow-hidden"
         style={{ display: "inline-block" }}
       >
         <AnimatePresence mode="wait" custom={direction}>
