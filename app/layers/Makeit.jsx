14c45a0393581245353f51f9e27e8d401791a100
akash100a <arakash968786@gmail.com>
2025-08-06 18:36:16 +0600

    update

--- app/layers/Makeit.jsx
+++ app/layers/Makeit.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
-import MakeitImg from "@/public/makeit.webp";
-import Image from 'next/image';
-const Makeit = () => {
-  return (
-      <div className="makeit py-18 h-[800px]">
-        {/* <Makeit /> */}
-        <Image className="w-full h-full bg-cover" src={MakeitImg} alt="makeit" />
-      </div>
-  )
-}
+"use client";
 
-export default Makeit
+import MakeitImg from "@/public/makeit.webp";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import Image from "next/image";
+import { useRef } from "react";
+
+const Makeit = () => {
+  const makeitRef = useRef(null);
+
+  // Scroll-based image scale animation
+  const { scrollYProgress } = useScroll({
+    target: makeitRef,
+    offset: ["start end", "end start"],
+  });
+
+  const scale = useSpring(useTransform(scrollYProgress, [0, 1], [1.2, 1]), {
+    stiffness: 200,
+    damping: 30,
+    mass: 0.5,
+  });
+
+  return (
+    <motion.div
+      ref={makeitRef}
+      style={{ scale }}
+      className="makeit py-18 h-[800px]"
+    >
+      {/* <Makeit /> */}
+      <Image className="w-full h-full bg-cover" src={MakeitImg} alt="makeit" />
+    </motion.div>
+  );
+};
+
+export default Makeit;
