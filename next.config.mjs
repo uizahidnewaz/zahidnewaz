a346e54df931b4fd1d8a91045151f5ae40e8820a
akash100a <arakash968786@gmail.com>
2025-08-07 12:34:03 +0600

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,18 @@
-import { createRequire } from 'module';
+import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [{ key: "X-Robots-Tag", value: "index, follow" }],
+      },
+    ];
+  },
   webpack(config) {
-    config.resolve.alias['framer-motion'] = require.resolve('framer-motion');
+    config.resolve.alias["framer-motion"] = require.resolve("framer-motion");
     return config;
   },
 };
