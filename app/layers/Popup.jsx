65bb8ab6f5ae5be93a6923af9d66a28f5ca0f038
akash100a <arakash968786@gmail.com>
2025-08-10 19:13:10 +0600

    update

--- app/layers/Popup.jsx
+++ app/layers/Popup.jsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
-
+import PopupImg from "@/public/popup.svg"; 
+import Image from "next/image";
 const Popup = () => {
   return (
-    <div className="popup_box absolute bottom-10 left-10 w-[677px] h-[327px] bg-[var(--color-white-solid)] text-black rounded-[12px] z-50 p-6">
-      <h2 className="popup_heading">Currently a Work In Progress</h2>
+    <div className="popup_box fixed bottom-10 left-10 w-[677px] bg-[var(--color-white-solid)] text-black rounded-[12px] z-50 p-6">
+        <Image src={PopupImg} alt="Popup Image" className="popup_image" />
+      <h2 className="popup_heading mt-5 mb-2">Currently a Work In Progress</h2>
       <p className="popup_paragraph">
         My portfolio is currently a work in progress. Real-world projects will
         be added here soon. In the meantime, you can explore other concept-based
