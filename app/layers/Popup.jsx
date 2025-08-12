2cdf2d97248174bf01f8ca72278a57546eb578a1
akash100a <arakash968786@gmail.com>
2025-08-13 00:04:55 +0600

    update

--- app/layers/Popup.jsx
+++ app/layers/Popup.jsx
@@ -29,7 +29,7 @@ const Popup = () => {
       <AnimatePresence>
         {isVisible && (
           <motion.div
-            className="popup_box fixed bottom-10 left-1/2 translate-x-[-50%] w-[96%] md:w-[677px] bg-[var(--color-white-solid)] text-black rounded-[12px] z-50 p-6 shadow-lg"
+            className="popup_box fixed top-1/2 translate-y-[-85%] left-1/2 translate-x-[-50%] w-[96%] md:w-[677px] bg-[var(--color-white-solid)] text-black rounded-[12px] z-50 p-6 shadow-lg"
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: 50 }}
