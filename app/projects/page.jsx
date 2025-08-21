d0e820414ff060fb0db94859888a5a0a523865e7
akash100a <arakash968786@gmail.com>
2025-08-21 21:15:20 +0600

    update

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -10,6 +10,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRef } from "react";
 import Container from "../components/Container";
+import ExperienceGallery from "../layers/ExperienceGallery";
 
 const page = () => {
   const sellectedref = useRef(null);
@@ -85,49 +86,52 @@ const page = () => {
   ];
 
   return (
-    <Container>
-      <h2 className="work_heading py-[96px] md:py-[80px] sm:py-[64px] xs:py-[48px]">
-        Projects
-      </h2>
-      <div className="gallery" ref={sellectedref}>
-        <div className="">
-          <motion.div
-            style={{ opacity }}
-            variants={containerVariants}
-            initial="hidden"
-            animate="show"
-            className="flex flex-col lg:flex-row items-center lg:items-start justify-center lg:justify-between gap-[24px] xs:gap-[28px] sm:gap-[30px] md:gap-[35px] lg:gap-[30px] w-full flex-wrap"
-          >
-            {projects.map((project) => (
-              <motion.div
-                key={project.id}
-                className="work_item cursor-pointer w-full lg:w-[48%]"
-                whileHover={{ scale: 1.05 }}
-                transition={{ duration: 0.3, ease: "easeOut" }}
-                variants={childVariants}
-              >
-                <Link
-                  href={project.link}
-                  className="block w-full aspect-[1.46/1] lg:h-[398px] md:h-[350px] sm:h-[300px] xs:h-[240px] overflow-hidden rounded-[3px] work_img"
+    <>
+      <Container>
+        <h2 className="work_heading py-[96px] md:py-[80px] sm:py-[64px] xs:py-[48px]">
+          Projects
+        </h2>
+        <div className="gallery" ref={sellectedref}>
+          <div className="">
+            <motion.div
+              style={{ opacity }}
+              variants={containerVariants}
+              initial="hidden"
+              animate="show"
+              className="flex flex-col lg:flex-row items-center lg:items-start justify-center lg:justify-between gap-[24px] xs:gap-[28px] sm:gap-[30px] md:gap-[35px] lg:gap-[30px] w-full flex-wrap"
+            >
+              {projects.map((project) => (
+                <motion.div
+                  key={project.id}
+                  className="work_item cursor-pointer w-full lg:w-[48%]"
+                  whileHover={{ scale: 1.05 }}
+                  transition={{ duration: 0.3, ease: "easeOut" }}
+                  variants={childVariants}
                 >
-                  <Image
-                    className="w-full h-full object-fit-contain"
-                    src={project.image}
-                    alt={project.alt}
-                    quality={100}
-                    priority={project.id === "01" || project.id === "02"}
-                  />
-                </Link>
-                <div className="mt-2 xs:mt-2.5 sm:mt-3 lg:mt-4 flex justify-between items-center w-full">
-                  <h3 className="work_subtitle">{project.title}</h3>
-                  <p className="work_number">{project.id}</p>
-                </div>
-              </motion.div>
-            ))}
-          </motion.div>
+                  <Link
+                    href={project.link}
+                    className="block w-full aspect-[1.46/1] lg:h-[398px] md:h-[350px] sm:h-[300px] xs:h-[240px] overflow-hidden rounded-[3px] work_img"
+                  >
+                    <Image
+                      className="w-full h-full object-fit-contain"
+                      src={project.image}
+                      alt={project.alt}
+                      quality={100}
+                      priority={project.id === "01" || project.id === "02"}
+                    />
+                  </Link>
+                  <div className="mt-2 xs:mt-2.5 sm:mt-3 lg:mt-4 flex justify-between items-center w-full">
+                    <h3 className="work_subtitle">{project.title}</h3>
+                    <p className="work_number">{project.id}</p>
+                  </div>
+                </motion.div>
+              ))}
+            </motion.div>
+          </div>
         </div>
-      </div>
-    </Container>
+      </Container>
+      <ExperienceGallery />
+    </>
   );
 };
 
